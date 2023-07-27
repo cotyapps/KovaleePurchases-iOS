06cab06bdf9e7ee433653d3722fd9dc340ce4528
fto-k <fto@kovalee.app>
2023-07-27 14:58:48 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# KovaleePurchases
+
+A description of this package.
---
+++ Sources/KovaleePurchases/KovaleePurchases.swift
@@ -0,0 +1,6 @@
+public struct KovaleePurchases {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
