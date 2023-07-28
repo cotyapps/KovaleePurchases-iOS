a6735abf0f65210c046471b297bfecf16d04ee8c
fto-k <fto@kovalee.app>
2023-07-28 15:48:22 +0200

    moved Purchase capabitilies for KovaleeSDK to specific Swift Package

--- Package.swift
+++ Package.swift
@@ -1,28 +1,33 @@
-// swift-tools-version: 5.8
+// swift-tools-version: 5.5
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "KovaleePurchases",
+	defaultLocalization: "en",
+	platforms: [
+		.iOS(.v14)
+	],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "KovaleePurchases",
-            targets: ["KovaleePurchases"]),
+            targets: [
+				"KovaleePurchases"
+			]
+		)
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+		.package(url: "https://github.com/cotyapps/Kovalee-iOS-SDK", branch: "sdk-spm-split"),
+		.package(url: "https://github.com/RevenueCat/purchases-ios", from: Version(4, 25, 0))
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "KovaleePurchases",
-            dependencies: []),
-        .testTarget(
-            name: "KovaleePurchasesTests",
-            dependencies: ["KovaleePurchases"]),
+            dependencies: [
+				.product(name: "KovaleeSDK", package: "Kovalee-iOS-SDK"),
+				.product(name: "RevenueCat", package: "purchases-ios")
+			]
+		)
     ]
 )
